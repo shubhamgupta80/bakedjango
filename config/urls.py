7f51ab4b7ee0dd9cc54a25afbbed9187d7a2a2c3
shubhamgupta80 <shabugupta80@gmail.com>
2024-04-08 15:27:14 +0530

    my work

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,21 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# urls.py
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from shop.views import contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('about/', TemplateView.as_view(template_name='about.html'),name='about')
+    path('page', TemplateView.as_view(template_name='page1.html'), name='page1'),
+    path('vendor/login', TemplateView.as_view(template_name='login.html'), name='vendor_login'),
+    path('contact', contact_view , name='contact'),
+    path('base', TemplateView.as_view(template_name='base.html'), name='base'),
+    path('pricing', TemplateView.as_view(template_name='pricing.html'), name='pricing'),
+    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
+    
 ]
 
 if settings.DEBUG:
