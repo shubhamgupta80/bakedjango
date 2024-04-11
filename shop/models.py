ef409879c6b356dc096e40684aae1c1676c1e806
shubhamgupta80 <shabugupta80@gmail.com>
2024-04-11 22:16:39 +0530

    new change

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     image = models.ImageField(upload_to='category')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -19,3 +20,21 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Dish(models.Model):
+    dish_type_choices = (
+        ('Veg', 'Veg'),
+        ('vegan', 'Vegan'),
+    )
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='dish')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    dish_type = models.CharField(max_length=10, choices=dish_type_choices)
+    available = models.BooleanField(default=True)
+    best_seller = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
